144ad884b750d04dc797efa6a1837e54dc3fdf57
Cameron Johnston <cameron@needle.com>
2013-02-18 09:28:21 -0700

    setting runit cookbook version dependency to ~> 1.0.4

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,11 @@ maintainer_email "alex@howells.me"
 license          "Apache 2.0"
 description      "Installs and configures Sickbeard onto a node"
 version          "1.0.0"
- 
+
 %w{ ubuntu }.each do |os|
   supports os
 end
 
 depends "bluepill"
-depends "runit"
+depends "runit", '~> 1.0.4'
 depends "git"
